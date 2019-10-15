84bb6635d0c1d45de8cd3d2b47fde0b485a9c954
Jakub Szewczyk <kubasz51@gmail.com>
2019-10-15 11:48:59 +0100

    Simple lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,3 @@
-
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Default, Hash)]
 pub struct SourceLocation {
     pub index: usize,
@@ -9,7 +8,9 @@ pub struct SourceLocation {
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum TokenKind {
     Identifier,
-    Number(i64),
+    Number,
+    Symbol,
+    StringLiteral,
     LParen,
     RParen,
     LBracket,
@@ -17,6 +18,7 @@ pub enum TokenKind {
     LBrace,
     RBrace,
     Dot,
+    Colon,
     AssignSeq,
     AssignConc,
     // block headers (always, etc.)
@@ -61,8 +63,21 @@ pub fn lex_source(source: &str) -> Result<Vec<Token>, String> {
     let mut v: Vec<Token> = Vec::new();
     let mut state = LexerState {
         remaining_source: source,
-        loc: SourceLocation::default()
+        loc: SourceLocation::default(),
     };
+    l_whitespace(&mut state)?;
+    while !state.remaining_source.is_empty() {
+        if let Some(t) = l_number(&mut state)? {
+            v.push(t);
+        } else if let Some(t) = l_identifier_or_keyword(&mut state)? {
+            v.push(t);
+        } else if let Some(t) = l_string(&mut state)? {
+            v.push(t);
+        } else if let Some(t) = l_symbol(&mut state)? {
+            v.push(t);
+        }
+        l_whitespace(&mut state)?;
+    }
     Ok(v)
 }
 
@@ -74,45 +89,121 @@ struct LexerState<'s> {
 
 type LexResult<'s> = Result<Option<Token<'s>>, String>;
 
-fn l_identifier_or_keyword(state: &mut LexerState) -> LexResult {
+fn l_whitespace(state: &mut LexerState) -> Result<(), String> {
+    while !state.remaining_source.is_empty() {
+        let src = state.remaining_source;
+        if src.starts_with("//") {
+            let len = src.chars().skip(2).take_while(|c| *c != '\n').count();
+            state.loc.index += len + 3;
+            state.loc.column = 0;
+            state.loc.line += 1;
+            state.remaining_source = &src[len + 3..];
+        } else if src.starts_with("/*") {
+            let len = src
+                .find("*/")
+                .ok_or_else(|| format!("/* comment not terminated at {:?}", state.loc))?;
+            state.loc.index += len + 2;
+            for c in src[0..len].chars() {
+                if c == '\n' {
+                    state.loc.column = 0;
+                    state.loc.line += 1;
+                } else {
+                    state.loc.column += 1;
+                }
+            }
+            state.remaining_source = &src[len + 2..];
+        } else if src.chars().nth(0).unwrap().is_ascii_whitespace() {
+            state.loc.index += 1;
+            if state.remaining_source.chars().nth(0).unwrap() == '\n' {
+                state.loc.column = 0;
+                state.loc.line += 1;
+            } else {
+                state.loc.column += 1;
+            }
+            state.remaining_source = &src[1..];
+        } else {
+            return Ok(());
+        }
+    }
+    Ok(())
+}
+
+fn l_string<'s>(state: &mut LexerState<'s>) -> LexResult<'s> {
+    let src = state.remaining_source;
+    if !src.starts_with('"') {
+        return Ok(None);
+    }
+    let len = src.chars().skip(1).take_while(|c| *c != '"').count();
+    let sstr = &src[1..=len];
+    let location = state.loc;
+    state.loc.index += len + 2;
+    state.loc.column += len + 2;
+    state.remaining_source = &src[len + 2..];
+    Ok(Some(Token {
+        kind: TokenKind::StringLiteral,
+        location,
+        instance: sstr,
+    }))
+}
+
+fn l_identifier_or_keyword<'s>(state: &mut LexerState<'s>) -> LexResult<'s> {
     let src = state.remaining_source;
     if src.starts_with('\\') {
         // escaped_identifier
-        let len = src.iter().skip(1)
+        let len = src
+            .chars()
+            .skip(1)
             .take_while(|c| !c.is_ascii_whitespace())
             .count();
-        let id = &src[1..1+len];
+        let id = &src[1..=len];
         let location = state.loc;
         state.loc.index += len + 1;
         state.loc.column += len + 1;
-        state.remaining_source = &src[1+len..];
-        return Ok(Some(Token{
+        state.remaining_source = &src[1 + len..];
+        return Ok(Some(Token {
             kind: TokenKind::Identifier,
             location,
-            instance: id
+            instance: id,
         }));
     }
-    if !(src[0].is_ascii_alphabetic() || src[0] == '_') {
+    if !(src.chars().nth(0).unwrap().is_ascii_alphabetic() || src.chars().nth(0).unwrap() == '_') {
         return Ok(None);
     }
-    let len = src.iter()
-        .take_while(|c: &char| c.is_ascii_alphanumeric() || c == '$' || c == '_')
+    let len = src
+        .chars()
+        .take_while(|c: &char| c.is_ascii_alphanumeric() || *c == '$' || *c == '_')
         .count();
     let id = &src[0..len];
     let location = state.loc;
     state.loc.index += len;
     state.loc.column += len;
     state.remaining_source = &src[len..];
-    return Ok(Some(Token {
+    Ok(Some(Token {
         kind: keyword_kind(id),
         location,
-        instance: id
-    }));
-    Ok(None)
+        instance: id,
+    }))
 }
 
-fn l_number(state: &mut LexerState) -> LexResult {
-    //
+fn l_number<'s>(state: &mut LexerState<'s>) -> LexResult<'s> {
+    let src = state.remaining_source;
+    if !src.chars().nth(0).unwrap().is_ascii_digit() {
+        return Ok(None);
+    }
+    let len = src
+        .chars()
+        .take_while(|c| c.is_ascii_alphanumeric() || *c == '\'' || *c == '_' || *c == '?')
+        .count();
+    let nstr = &src[0..len];
+    let location = state.loc;
+    state.loc.index += len;
+    state.loc.column += len;
+    state.remaining_source = &src[len..];
+    Ok(Some(Token {
+        kind: TokenKind::Number,
+        location,
+        instance: nstr,
+    }))
 }
 
 fn keyword_kind(id: &str) -> TokenKind {
@@ -148,4 +239,60 @@ fn keyword_kind(id: &str) -> TokenKind {
         "endmodule" => KEndModule,
         _ => TokenKind::Identifier,
     }
+}
+
+fn l_symbol<'s>(state: &mut LexerState<'s>) -> LexResult<'s> {
+    if !state
+        .remaining_source
+        .chars()
+        .nth(0)
+        .unwrap()
+        .is_ascii_punctuation()
+    {
+        return Ok(None);
+    }
+    let t = match state.remaining_source.chars().nth(0).unwrap() {
+        '(' => Some(TokenKind::LParen),
+        ')' => Some(TokenKind::RParen),
+        '[' => Some(TokenKind::LBracket),
+        ']' => Some(TokenKind::RBracket),
+        '{' => Some(TokenKind::LBrace),
+        '}' => Some(TokenKind::RBrace),
+        '.' => Some(TokenKind::Dot),
+        ':' => Some(TokenKind::Colon),
+        _ => None,
+    };
+    if let Some(kind) = t {
+        let location = state.loc;
+        let instance = &state.remaining_source[0..1];
+        state.remaining_source = &state.remaining_source[1..];
+        state.loc.index += 1;
+        state.loc.column += 1;
+        return Ok(Some(Token {
+            kind,
+            instance,
+            location,
+        }));
+    }
+    let symlen = state
+        .remaining_source
+        .chars()
+        .take_while(|c| c.is_ascii_punctuation())
+        .count();
+    let symstr = &state.remaining_source[0..symlen];
+    let location = state.loc;
+    state.loc.index += symlen;
+    state.loc.column += symlen;
+    state.remaining_source = &state.remaining_source[symlen..];
+    let mut kind = TokenKind::Symbol;
+    if symstr.starts_with("<=") {
+        kind = TokenKind::AssignConc;
+    } else if symstr.starts_with('=') && !symstr.starts_with("==") {
+        kind = TokenKind::AssignSeq;
+    }
+    Ok(Some(Token {
+        kind,
+        instance: symstr,
+        location,
+    }))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::io::ErrorKind;
-use std::path::{PathBuf, Path};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
-mod lexer;
 mod ast;
+mod lexer;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name="verinject")]
+#[structopt(name = "verinject")]
 pub struct Options {
     #[structopt(name = "FILE", parse(from_os_str))]
     input_file: PathBuf,
@@ -23,25 +23,46 @@ impl Options {
 
     fn generate_defaults(&mut self) {
         if self.output_file.is_none() {
-            self.output_file = Some(self.input_file.with_file_name(self.input_file.file_name().unwrap_or_default() + "_injected"));
+            let mut name = self
+                .input_file
+                .file_name()
+                .unwrap_or_default()
+                .to_os_string();
+            name.push("_injected");
+            self.output_file = Some(self.input_file.with_file_name(&name));
         }
     }
 }
 
 fn main() -> std::io::Result<()> {
     let options = Options::read_cmd();
-    let input_file = match std::fs::read_to_string(options.input_file) {
+    let input_file = match std::fs::read_to_string(&options.input_file) {
         Ok(f) => f,
         Err(e) => match e.kind() {
             ErrorKind::NotFound => {
-                eprintln!("Could not find file `{}`", options.input_file);
+                eprintln!("Could not find file `{}`", options.input_file.display());
                 return Err(e);
             }
             _ => {
-                eprintln!("Could not read file `{}`: {:?}", options.input_file, e);
+                eprintln!(
+                    "Could not read file `{}`: {:?}",
+                    options.input_file.display(),
+                    e
+                );
                 return Err(e);
             }
-        }
+        },
     };
+    let lexed = lexer::lex_source(&input_file)
+        .map_err(|s| std::io::Error::new(ErrorKind::InvalidInput, s))?;
+    let mut pline = 0;
+    for t in &lexed {
+        while pline != t.location.line {
+            pline += 1;
+            println!();
+        }
+        print!("{:?}({}) ", t.kind, t.instance);
+    }
+    println!();
     Ok(())
 }
